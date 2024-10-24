dfd00a93c784bac305a6544bd13ad1b4c6ceb1a0
tkj <2067838730@qq.com>
2024-10-24 22:12:50 +0800

    项目配置微信验签、回复消息功能

--- s-pay-mall-web/src/main/java/cn/nju/edu/Application.java
+++ s-pay-mall-web/src/main/java/cn/nju/edu/Application.java
@@ -4,6 +4,7 @@ package cn.nju.edu;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @Configurable
